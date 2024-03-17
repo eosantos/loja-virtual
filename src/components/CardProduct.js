a18fbde69e21308dd7a5e705b96f03fdc6633604
eduardooliveira <eos.eduardooliveira@gmail.com>
2024-03-16 22:26:40 -0300

    Formatted price BRL

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -5,11 +5,15 @@ const Card = ({ src, title, price }) => {
   // Limitar o título a três palavras
   const truncatedTitle = title.split(' ').slice(0, 2).join(' ')
 
+  // Formatar o preço para o formato de moeda brasileira (Real)
+  const formattedPrice = Number(price).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })
+
   return (
     <div className="max-w-[350px] h-[450px] flex items-center justify-center">
       <div className="max-w-xs relative rounded-lg overflow-hidden shadow-lg m-10 flex flex-col">
-        {' '}
-        {/* Classe rounded-lg adicionada para bordas mais arredondadas */}
         <div className="w-[250px] h-[250px] relative">
           <Image
             src={src}
@@ -26,7 +30,8 @@ const Card = ({ src, title, price }) => {
           >
             {truncatedTitle}
           </div>
-          <p className="text-gray-700 text-base">${price}</p>
+          <p className="text-gray-700 text-base">{formattedPrice}</p>{' '}
+          {/* Usando o preço formatado */}
         </div>
       </div>
     </div>
